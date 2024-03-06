bbf4d297665f001f3db990a9fbcf8f05cc69b8fd
Alastair Longmuir <alastairlinwood@gmail.com>
2024-03-06 15:18:07 +0100

    set up all routes with authentication

--- routes/expense.routes.js
+++ routes/expense.routes.js
@@ -1,8 +1,63 @@
 const express = require("express");
 const router = express.Router();
+const Expense = require("../models/Expense.model")
+const mongoose = require('mongoose');
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+
+// POST /api/expenses - Create new expense
+router.post('/', isAuthenticated, (req, res, next) => {
+
+  const {expenseName, description, group, amount, payer, splits} = req.body;
+
+  Expense.create({expenseName, description, group, amount, payer, splits})
+    .then(newExpense => {
+      res.status(201).json(newExpense);
+    })
+    .catch(err => {
+      next(err);
+    })
+
+})
+
+
+// GET /api/expenses/:expenseId - Get expense by ID
+
+router.get("/:expenseId", isAuthenticated, (req, res, next) => {
+  const  { expenseId }  = req.params
+  const userId = req.payload._id;
+  
+  Expense.findOne({"$and": [{"_id": expenseId}, {"$or": [{"splits.userId": userId}, {"payer": userId}]}]})
+    .populate("splits.userId", "name")
+    .then(groups => {
+      res.status(200).json(groups)
+    })
+    .catch(err => {
+      next(err)
+    })
 });
+
+// PUT /api/groups/:groupId - Update group
+
+router.put("/:expenseId", isAuthenticated, (req, res, next) => {
+  const { expenseId }  = req.params;
+  const userId = req.payload._id;
+  
+  const { expenseName, description, group, amount, payer, splits } = req.body;
+
+  const updatedExpense = { expenseName, description, group, amount, payer, splits }
+
+  Expense.findOneAndUpdate({"$and": [{"_id": expenseId}, {"$or": [{"splits.userId": userId}, {"payer": userId}]}]}, updatedExpense, {new: true})
+    .populate("splits.userId", "name")
+    .then(updatedExpense => {
+      res.status(200).json(updatedExpense)
+    })
+    .catch(err => {
+      next(err)
+    })
+});
+
+
+
 
 module.exports = router;
--- routes/group.routes.js
+++ routes/group.routes.js
@@ -1,8 +1,76 @@
 const express = require("express");
 const router = express.Router();
+const Group = require("../models/Group.model")
+const mongoose = require('mongoose');
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+
+// GET /api/groups/user/ - Get all groups of user
+router.get("/user", isAuthenticated, (req, res, next) => {
+  // get User Id from the middleware
+  const userId = req.payload._id;
+   
+  Group.find({members: userId})
+    .populate("members", ["name", "email"])
+    .then(groups => {
+      res.status(200).json(groups)
+    })
+    .catch(err => {
+      next(err)
+    })
 });
+
+
+// GET /api/groups/:groupId - Get group by ID
+
+router.get("/:groupId", isAuthenticated, (req, res, next) => {
+  const  { groupId }  = req.params
+  const userId = req.payload._id;
+  
+  Group.findOne({$and: [{_id: groupId}, {members: userId}]})
+    .populate("members", ["name", "email"])
+    .then(groups => {
+      res.status(200).json(groups)
+    })
+    .catch(err => {
+      next(err)
+    })
+});
+
+// PUT /api/groups/:groupId - Update group
+
+router.put("/:groupId", isAuthenticated, (req, res, next) => {
+  const { groupId }  = req.params;
+  const userId = req.payload._id;
+  
+  const { groupName, members } = req.body;
+
+  const updatedGroup = { groupName, members }
+
+  Group.findOneAndUpdate({$and: [{_id: groupId}, {members: userId}]}, updatedGroup, {new: true})
+    .populate("members", ["name", "email"])
+    .then(updatedGroup => {
+      res.status(200).json(updatedGroup)
+    })
+    .catch(err => {
+      next(err)
+    })
+});
+
+
+// POST /api/groups - Create new group
+router.post('/', isAuthenticated, (req, res, next) => {
+
+  const {groupName, members} = req.body;
+
+  Group.create({groupName, members})
+    .then(newGroup => {
+      res.status(201).json(newGroup);
+    })
+    .catch(err => {
+      next(err);
+    })
+
+})
 
 module.exports = router;
