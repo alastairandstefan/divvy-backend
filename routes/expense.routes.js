910163b763121167480f08cb4630928910985986
Alastair Longmuir <alastairlinwood@gmail.com>
2024-03-07 11:29:51 +0100

    added route to get expenses by groupId

--- routes/expense.routes.js
+++ routes/expense.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Expense = require("../models/Expense.model")
 const mongoose = require('mongoose');
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
+const { default: GroupDetailsPage } = require("../../divvy-frontend/src/pages/GroupDetailsPage.jsx");
 
 
 // POST /api/expenses - Create new expense
@@ -29,15 +30,31 @@ router.get("/:expenseId", isAuthenticated, (req, res, next) => {
   
   Expense.findOne({"$and": [{"_id": expenseId}, {"$or": [{"splits.userId": userId}, {"payer": userId}]}]})
     .populate("splits.userId", "name")
-    .then(groups => {
-      res.status(200).json(groups)
+    .then(expense => {
+      res.status(200).json(expense)
     })
     .catch(err => {
       next(err)
     })
 });
 
-// PUT /api/groups/:groupId - Update group
+// GET /api/expenses/group/:groupId - Get expense by groupId
+
+router.get("/group/:groupId", isAuthenticated, (req, res, next) => {
+  const  { groupId }  = req.params
+  const userId = req.payload._id;
+  
+  Expense.findOne({"$and": [{"group": GroupDetailsPage}, {"$or": [{"splits.userId": userId}, {"payer": userId}]}]})
+    .populate("splits.userId", "name")
+    .then(expenses => {
+      res.status(200).json(expenses)
+    })
+    .catch(err => {
+      next(err)
+    })
+});
+
+// PUT /api/expenses/:expenseId - Update expense
 
 router.put("/:expenseId", isAuthenticated, (req, res, next) => {
   const { expenseId }  = req.params;
